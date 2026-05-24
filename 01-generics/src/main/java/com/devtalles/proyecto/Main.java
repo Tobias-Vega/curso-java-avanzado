de122adc16d8d36d804adb87308d555e9fd2beda
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-23 21:45:51 -0300

    Wildcards, extends y super

--- 01-generics/src/main/java/com/devtalles/proyecto/Main.java
+++ 01-generics/src/main/java/com/devtalles/proyecto/Main.java
@@ -1,6 +1,7 @@
 package com.devtalles.proyecto;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     static void main(String[] args) {
@@ -14,5 +15,48 @@ public class Main {
 
         MathUtils.sum(2, 2);
 
+        List<String> names = new ArrayList<>();
+
+        names.add("Tobías");
+        names.add("María");
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(2);
+
+        printList(names);
+        printList(numbers);
+
+        sumNumbers(numbers);
+
+        addNumbers(numbers);
+
+    }
+
+    public static void printList(List<?> list) {
+        for (Object o : list) {
+            System.out.println(o);
+        }
+    }
+
+    public static void sumNumbers(List<? extends Number> numbers) {
+        double sum = 0;
+
+        for (Number num : numbers) {
+            sum += num.doubleValue();
+        }
+        System.out.println(sum);
+    }
+
+    public static void addNumbers(List<? super Integer> numbers) {
+
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        Object num = numbers.get(0);
+
+        System.out.println(num);
     }
 }
