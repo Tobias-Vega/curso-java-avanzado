a28b9979562ece4eb2c119a80c25cbbeacd19220
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-27 23:24:34 -0300

    Proyecto final: El controlador

---
+++ 04-final-project-section-3/src/main/java/com/devtalles/proyecto/product/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.devtalles.proyecto.product.controller;
+
+import com.devtalles.proyecto.product.exceptions.InvalidProductException;
+import com.devtalles.proyecto.product.exceptions.ProductNotFoundException;
+import com.devtalles.proyecto.product.model.Product;
+import com.devtalles.proyecto.product.service.ProductService;
+import com.devtalles.proyecto.product.util.Validates;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    public void addProduct(Product product) throws InvalidProductException {
+        Validates.validateObject(product, "El product no puede ser un dato nulo");
+        productService.saveProduct(product);
+    }
+
+    public void removeProduct(Long id) throws ProductNotFoundException, InvalidProductException {
+        Validates.validateNumber(id, "El ID no puede ser nulo");
+        productService.deleteProduct(id);
+    }
+
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    public Optional<Product> getProductById(Long id) throws InvalidProductException {
+        Validates.validateNumber(id, "El ID no puede ser nulo");
+        return productService.getProductById(id);
+    }
+
+    public void updateProduct(Product product) throws ProductNotFoundException, InvalidProductException {
+        Validates.validateObject(product, "El product no puede ser un dato nulo");
+        productService.updateProduct(product);
+    }
+}
