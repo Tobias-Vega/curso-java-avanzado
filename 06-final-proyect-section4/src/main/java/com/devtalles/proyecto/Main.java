978f6e55089d2126b6f49b97031d695567e318c2
Tobias Vega <tobiasvega1210@gmail.com>
2026-05-29 16:51:49 -0300

    Proyecto final: el servicio - LogService

--- 06-final-proyect-section4/src/main/java/com/devtalles/proyecto/Main.java
+++ 06-final-proyect-section4/src/main/java/com/devtalles/proyecto/Main.java
@@ -1,6 +1,10 @@
 package com.devtalles.proyecto;
 
+import com.devtalles.proyecto.log.model.LogEntity;
+import com.devtalles.proyecto.log.service.LogService;
+
 import java.io.File;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +19,11 @@ public class Main {
                     "Asegúrate de crearla y poner archivos dentro");
             return;
         }
+
+        LogService service = new LogService();
+
+        List<LogEntity> entries = service.readLogsFromFile(logFiles[0].getAbsolutePath());
+
+        entries.forEach(System.out::println);
     }
 }
--- 06-final-proyect-section4/src/main/java/com/devtalles/proyecto/log/service/LogService.java
+++ 06-final-proyect-section4/src/main/java/com/devtalles/proyecto/log/service/LogService.java
@@ -1,4 +1,26 @@
 package com.devtalles.proyecto.log.service;
 
+import com.devtalles.proyecto.log.model.LogEntity;
+import com.devtalles.proyecto.log.util.LogParser;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Optional;
+
 public class LogService {
+
+    public List<LogEntity> readLogsFromFile(String filePath) {
+        try {
+            return Files.lines(Path.of(filePath))
+                    .map(LogParser::parseLine)
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .toList();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return List.of();
+        }
+    }
 }
