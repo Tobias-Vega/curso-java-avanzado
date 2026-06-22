2dbabc39485c531a9d473c862ea475e3bd1936b2
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-22 12:39:30 -0300

    Observables: flat VS flatMap

---
+++ 07-section-reactividad/src/main/java/com/devtalles/proyecto/encadenamientoAvanzado/ChainingExample.java
@@ -0,0 +1,36 @@
+package com.devtalles.proyecto.encadenamientoAvanzado;
+
+import com.devtalles.proyecto.task.student.model.Student;
+import io.reactivex.rxjava3.core.Observable;
+
+public class ChainingExample {
+    public static void main(String[] args) {
+        Observable<Student> studentObservable = Observable.just(
+                new Student("Mario", 13),
+                new Student("Juan", 33),
+                new Student("Estela", 43)
+        );
+
+//        studentObservable
+//                .filter(student -> student.getAge() > 21)
+//                .map(student -> student.getName().toUpperCase())
+//                .subscribe(System.out::println);
+
+        studentObservable
+                .filter(student -> student.getAge() > 21)
+                .flatMap(student -> getSubjectsPerStudent(student.getName()))
+                .subscribe(
+                        s -> System.out.println("Materia: " + s),
+                        error -> System.out.println(error.getMessage()),
+                        () -> System.out.println("Fin")
+                );
+    }
+
+    public static Observable<String> getSubjectsPerStudent(String name) {
+        if (name.equalsIgnoreCase("Juan")) {
+            return Observable.just("Programación 3", "Lenguaje");
+        }
+
+        return Observable.empty();
+    }
+}
