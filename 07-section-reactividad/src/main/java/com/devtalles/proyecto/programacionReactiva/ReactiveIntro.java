0fa46494d3380d85250631286a017724e120cf7b
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-22 10:50:14 -0300

    Observable: create()

--- 07-section-reactividad/src/main/java/com/devtalles/proyecto/programacionReactiva/ReactiveIntro.java
+++ 07-section-reactividad/src/main/java/com/devtalles/proyecto/programacionReactiva/ReactiveIntro.java
@@ -1,16 +1,45 @@
 package com.devtalles.proyecto.programacionReactiva;
 
+import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+
+import java.util.Scanner;
 
 public class ReactiveIntro {
     public static void main(String[] args) {
         // Primer ejemplo
-        Observable<String> courseStream = Observable.just("HTML", "CSS", "JAVA", "C");
+//        Observable<String> courseStream = Observable.just("HTML", "CSS", "JAVA", "C");
+//
+//        courseStream.subscribe(
+//                item -> System.out.println("Recibido: " + item),
+//                error -> System.out.println(error.getMessage()),
+//                () -> System.out.println("Finalizado...")
+//        );
 
-        courseStream.subscribe(
-                item -> System.out.println("Recibido: " + item),
-                error -> System.out.println(error.getMessage()),
-                () -> System.out.println("Finalizado...")
+        ObservableOnSubscribe subscribe = emitter -> {
+
+            Scanner scanner = new Scanner(System.in);
+            String input;
+
+            while (true) {
+
+                System.out.println("Ingrese un nombre o 'salir' para dejar la app");
+                input = scanner.nextLine();
+                if (input.equalsIgnoreCase("salir")) {
+                    break;
+                }
+
+                emitter.onNext(input);
+
+            }
+        };
+
+        Observable<String> names = Observable.create(subscribe);
+
+        names.subscribe(
+                item -> System.out.println("Name: " + item)
         );
     }
 }
