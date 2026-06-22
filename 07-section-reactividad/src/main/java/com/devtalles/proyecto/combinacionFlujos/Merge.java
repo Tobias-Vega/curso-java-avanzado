853c9c4aee43df758c93dc3fdf1043c2be22d30d
Tobias Vega <tobiasvega1210@gmail.com>
2026-06-22 12:13:35 -0300

    Combinando flujos con: merge, concat y zip

---
+++ 07-section-reactividad/src/main/java/com/devtalles/proyecto/combinacionFlujos/Merge.java
@@ -0,0 +1,39 @@
+package com.devtalles.proyecto.combinacionFlujos;
+
+import com.devtalles.proyecto.task.student.model.Student;
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+public class Merge {
+    public static void main(String[] args) throws InterruptedException {
+
+        // Ejemplo merge - No bloqueante
+//        Observable<String> students = Observable.just("Mario", "Ana", "Juana", "Fernando")
+//                .delay(1, TimeUnit.SECONDS);
+//
+//        Observable<String> teachers = Observable.just("Prof. Carlos", "Prof. Ana", "Prof. Juan", "Prof. Ricardo");
+//
+//        Observable.merge(students, teachers).subscribe(System.out::println);
+//
+//        Thread.sleep(2000);
+
+        // Ejemplo concat - Secuencial
+//        Observable<String> students = Observable.just("Mario", "Ana", "Juana", "Fernando")
+//                .delay(2, TimeUnit.SECONDS);
+//
+//        Observable<String> teachers = Observable.just("Prof. Carlos", "Prof. Ana", "Prof. Juan", "Prof. Ricardo");
+//
+//        Observable.concat(students, teachers).subscribe(System.out::println);
+//
+//        Thread.sleep(3000);
+
+        Observable<String> students = Observable.just("Mario", "Ana", "Juana", "Fernando");
+        Observable<Integer> ages = Observable.just(20, 30, 40, 50);
+
+        Observable.zip(students, ages, (name, age) -> new Student(name, age))
+                .subscribe(student -> System.out.println(student));
+
+
+    }
+}
